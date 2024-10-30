774427717316b1f6f5a9254c342a9a374e8aa323
Yousuf El Refai <yelrefai@uoguelph.ca>
2024-10-30 16:27:16 -0400

    Added handle cases for NULL and empty string

--- src/com/jwetherell/algorithms/strings/StringFunctions.java
+++ src/com/jwetherell/algorithms/strings/StringFunctions.java
@@ -13,6 +13,11 @@ public class StringFunctions {
     private static final char SPACE = ' ';
 
     public static final String reverseWithStringConcat(String string) {
+        if (string == null || string.isEmpty()){
+            return "";
+        }
+            
+            
         String output = new String();
         for (int i = (string.length() - 1); i >= 0; i--) {
             output += (string.charAt(i));
@@ -21,6 +26,9 @@ public class StringFunctions {
     }
 
     public static final String reverseWithStringBuilder(String string) {
+        if (string == null || string.isEmpty()){
+            return "";
+        }
         final StringBuilder builder = new StringBuilder();
         for (int i = (string.length() - 1); i >= 0; i--) {
             builder.append(string.charAt(i));
